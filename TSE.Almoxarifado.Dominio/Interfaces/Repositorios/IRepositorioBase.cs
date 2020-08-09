568c492a13517aae9aaa42d59b3838fa194636ec
Gustavo <gduartemenezes@gmail.com>
2020-08-09 16:47:19 -0300

    Add Interface with possible operations to any entity

---
+++ TSE.Almoxarifado.Dominio/Interfaces/Repositorios/IRepositorioBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSE.Almoxarifado.Dominio.Interfaces.Repositorios
+{
+    public interface IRepositorioBase<T> where T : class
+    {
+        T ObtenhaPorId(Guid id);
+
+        IEnumerable<T> ObtenhaTodos();
+
+        IEnumerable<T> Filtre(Func<T, bool> expressaoFiltro);
+
+        void Adicione(T objeto);
+        void Atualize(T objeto);
+        void Remova(T objeto);
+    }
+}
