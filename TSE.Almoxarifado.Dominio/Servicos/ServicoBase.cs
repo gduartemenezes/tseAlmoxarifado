20fcb680e20359e5bd95205e7751aab88082b21e
Gustavo <gduartemenezes@gmail.com>
2020-08-09 16:56:22 -0300

    Create Base service to inherit UnityOfWork parameters

---
+++ TSE.Almoxarifado.Dominio/Servicos/ServicoBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSE.Almoxarifado.Dominio.Interfaces.UnidadeDeTrabalho;
+
+namespace TSE.Almoxarifado.Dominio.Servicos
+{
+    public class ServicoBase
+    {
+        protected ITSEUnidadeDeTrabalho UnidadeDeTrabalho { get; set; }
+
+        public ServicoBase(ITSEUnidadeDeTrabalho unidadeDeTrabalho)
+        {
+            this.UnidadeDeTrabalho = unidadeDeTrabalho;
+        }
+    }
+}
