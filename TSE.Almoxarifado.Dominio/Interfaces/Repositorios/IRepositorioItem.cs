0f1fb21d33fefa066dee04f663d95611b3af6736
Gustavo <gduartemenezes@gmail.com>
2020-08-09 16:48:42 -0300

    Add Item interfarce, inherits from base Interface

---
+++ TSE.Almoxarifado.Dominio/Interfaces/Repositorios/IRepositorioItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSE.Almoxarifado.Dominio.Entidades;
+
+namespace TSE.Almoxarifado.Dominio.Interfaces.Repositorios
+{
+     public interface IRepositorioItem : IRepositorioBase<Item>
+    {
+    }
+}
