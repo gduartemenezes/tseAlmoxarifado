b0b0e9fefb14922f6aa3e1492ecc0fca4bb13d1e
Gustavo <gduartemenezes@gmail.com>
2020-08-09 17:27:26 -0300

    Create DBContext to project and enable migrations

---
+++ TSE.Infra.Data/Contexto/AlmoxarifadoContexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSE.Infra.Data.Contexto
+{
+     public class AlmoxarifadoContexto : DbContext
+    {
+       public AlmoxarifadoContexto() : base("Server = localhost; Database=master;Trusted_Connection=True;")
+        {
+          
+        }
+    }
+}
---
+++ TSE.Infra.Data/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace TSE.Infra.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TSE.Infra.Data.Contexto.AlmoxarifadoContexto>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TSE.Infra.Data.Contexto.AlmoxarifadoContexto context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
