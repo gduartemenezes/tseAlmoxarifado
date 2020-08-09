5fcca98bbb5ad31d12dd0c379f3ce08676bb459e
Gustavo <gduartemenezes@gmail.com>
2020-08-09 17:36:34 -0300

    Change conventions in DBCreation

--- TSE.Infra.Data/Contexto/AlmoxarifadoContexto.cs
+++ TSE.Infra.Data/Contexto/AlmoxarifadoContexto.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TSE.Infra.Data.Migrations;
 
 namespace TSE.Infra.Data.Contexto
 {
@@ -11,7 +13,23 @@ namespace TSE.Infra.Data.Contexto
     {
        public AlmoxarifadoContexto() : base("Server = localhost; Database=master;Trusted_Connection=True;")
         {
-          
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<AlmoxarifadoContexto, Configuration>());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
+
+
         }
     }
 }
+
