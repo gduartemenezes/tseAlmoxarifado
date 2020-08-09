beb0e4a84323e29059ce0622fb5c3344bcbcdd1e
Gustavo <gduartemenezes@gmail.com>
2020-08-09 16:41:57 -0300

    Add GitIgnore file and first entity

---
+++ TSE.Almoxarifado.Dominio/Entidades/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSE.Almoxarifado.Dominio.Entidades
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+
+        public string NomeItem { get; set; }
+        
+        public int Quantidade { get; set; }
+    }
+}
